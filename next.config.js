34e79a53101af1060de6e1fb028629f3893b0809
Fatha Adillah <fatha.adillah12@gmail.com>
2026-01-28 22:05:52 +0700

    Dockerfile

--- next.config.js
+++ next.config.js
@@ -1,5 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: 'standalone',
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: 'https://resome-fatha.fritz.my.id',
+          },
+        ],
+      },
+    ];
+  },
+}
 
 module.exports = nextConfig
 
